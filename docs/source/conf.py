65ead898b730fede7bf7a2fb35c84e39a5745fd1
Jonas Winklmann <jonas.winklmann@tum.de>
2025-06-12 14:10:46 +0200

    Updated sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../resorting_pip/resorting/'))
+sys.path.insert(0, os.path.abspath('../../resorting_pip/hipars/'))
 import sphinx_rtd_theme
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -11,7 +11,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Resorting'
+project = 'HiPARS'
 copyright = '2024, Jonas Winklmann'
 author = 'Jonas Winklmann'
 release = '0.0.1'
